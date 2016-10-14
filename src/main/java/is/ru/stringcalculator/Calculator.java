17dca2686ca101e36f8650685742167d9f00e751
armannari <armann15@ru.is>
2016-10-14 17:06:18 +0000

    final push missing last two requirements

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,19 +1,42 @@
 package is.ru.stringcalculator;
 
-public class Calculator {
+import java.util.ArrayList;
+import java.util.List;
 
-	public static final int add(final String numbers) {
-		
-		int returnValue = 0;
-		String[] numbersArray = numbers.split(",");
-		if (numbersArray.length > 2) {
-			throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+public class Calculator {
+	
+	public static int add(final String numbers) {
+
+		String delimiter = ",|\n";
+		String numbersWithoutDelimiter = numbers;
+		if (numbers.startsWith("//")) {
+			int delimiterIndex = numbers.indexOf("//") + 2;
+			delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
+			numbersWithoutDelimiter = numbers.substring(numbers.indexOf("\n") + 1);	
 		}
+		return add(numbersWithoutDelimiter, delimiter);
+	}
+	
+	private static int add(final String numbers, final String delimiter) {
+
+		int valueToReturn = 0;
+		String[] numbersArray = numbers.split(delimiter);
+		List<Integer> numbersLowerThanZero = new ArrayList<Integer>();
+
 		for (String number : numbersArray) {
 			if (!number.trim().isEmpty()) {
-				returnValue += Integer.parseInt(number);
+				int numberInt = Integer.parseInt(number.trim());
+				if (numberInt < 0) {
+					numbersLowerThanZero.add(numberInt);
+				} else if (numberInt <= 1000) {	
+					valueToReturn += numberInt;
+				}
 			}
 		}
-		return returnValue;
+		if (numbersLowerThanZero.size() > 0) {
+			throw new RuntimeException("Numbers cannot be lower than 0: " + numbersLowerThanZero.toString());
+		}
+		return valueToReturn;		
 	}
+	
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,33 +5,55 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	@Test(expected = RuntimeException.class)
-	public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
-		Calculator.add("1,2,3");
-	}
-
 	@Test
-	public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
+	public final void addTwoNumbers() {
 		Calculator.add("1,2");
 		Assert.assertTrue(true);
 	}
 
 	@Test(expected = RuntimeException.class)
-	public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+	public final void addNonNumber() {
 		Calculator.add("1,X");
 	}
-
+	
 	@Test
-	public final void whenEmptyStringIsUsedThenReturnValueIs0() {
+	public final void addEmptyString() {
 		Assert.assertEquals(0, Calculator.add(""));
 	}
+	
+	@Test
+	public final void addOneNumber() {
+		Assert.assertEquals(5, Calculator.add("5"));
+	}
 
 	@Test
-	public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
-		Assert.assertEquals(3, Calculator.add("3"));
+	public final void addTwoNumbersReturnsSum() {
+		Assert.assertEquals(4+1, Calculator.add("4,1"));
 	}
+	
 	@Test
-	public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
-		Assert.assertEquals(3+6, Calculator.add("3,6"));
+	public final void addAnyNumberOfNumbers() {
+		Assert.assertEquals(6+7+8+9+10+11, Calculator.add("6,7,8,9,10,11"));
 	}
+
+	@Test
+	public final void addNumbersWithLinesBetween() {
+		Assert.assertEquals(1,2,3, Calculator.add("1,2\n3"));
+	}
+
+	@Test
+	public final void AddNumbersWithDelemiter() {
+		Assert.assertEquals(4+5+6, Calculator.add("//;\n4;5;6"));
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public final void addNegativeNumber() {
+		Calculator.add("1,-2,3,4,5,6");
+	}
+
+	@Test
+	public final void addNumberGreaterThan1000() {
+		Assert.assertEquals(1+1000+2, Calculator.add("1,1000,1010,2,1009"));
+	}
+
 }
